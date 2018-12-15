3494cdd5a8d0442920f7027aafec870ebcf78abd
shilpipawar <shilpipawar@gmail.com>
2018-12-15 13:44:55 -0600

    Code Added

--- server.js
+++ server.js
@@ -15,17 +15,17 @@ app.use(express.json());
 // Reservation (DATA)
 // =============================================================
 var counterID = 1;
-function Customer(name,email,phone,uniqueid){
+function Customer(name, email, phone, uniqueid) {
   this.Name = name,
-  this.Email = email,
-  this.Phone = phone,
-  this.UniqueId = parseInt(counterID),
-  this.printObj = function(){
-console.log("Name: " + this.Name
-+ " Email: " + this.Email
-+ " Phone: " + this.Phone,
-+ " Unique Id:" + this.UniqueId)
-  }
+    this.Email = email,
+    this.Phone = phone,
+    this.UniqueId = parseInt(counterID),
+    this.printObj = function () {
+      console.log("Name: " + this.Name
+        + " Email: " + this.Email
+        + " Phone: " + this.Phone,
+        + " Unique Id:" + this.UniqueId)
+    }
 };
 // var customer1 = new Customer("John","j@gmail.com","5555555656", 1);
 // customer1.printObj();
@@ -37,22 +37,22 @@ var waitList = [];
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "home.html"));
 });
 
-app.get("/tables", function(req, res) {
+app.get("/tables", function (req, res) {
   res.sendFile(path.join(__dirname, "tables.html"));
 });
 
 // Displays all characters
-app.get("/reservations", function(req, res) {
- // return res.json(characters);
- res.sendFile(path.join(__dirname, "reservation.html"));
+app.get("/reservations", function (req, res) {
+  // return res.json(characters);
+  res.sendFile(path.join(__dirname, "reservation.html"));
 });
 
 // Displays a single character, or returns false
-app.get("/api/reservations/:reservation", function(req, res) {
+app.get("/api/reservations/:reservation", function (req, res) {
   var chosen = req.params.reservation;
 
   console.log(chosen);
@@ -62,12 +62,12 @@ app.get("/api/reservations/:reservation", function(req, res) {
       return res.json(reservations[i]);
     }
   }
-  
+
   return res.json(false);
 });
 
 // Create New Characters - takes in JSON input
-app.post("/api/reservations", function(req, res) {
+app.post("/api/reservations", function (req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
   var newReservation = req.body;
@@ -79,18 +79,20 @@ app.post("/api/reservations", function(req, res) {
   console.log(newReservation);
 
   if (reservations.length > 4) {
+    newReservation.wait = true;
     waitList.push(newReservation);
     console.log(waitList);
   } else {
+    newReservation.wait = false;
     reservations.push(newReservation);
     console.log(reservations);
   }
-  
+
   res.json(newReservation);
 });
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
